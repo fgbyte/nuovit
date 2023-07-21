61cfeba9269feae59dd5584306d85b2e7428998f
fgbyte <garciabayonf@gmail.com>
2023-07-21 18:03:15 -0400

    colors & fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,6 @@ module.exports = {
   content: ["./dist/*.html"],
   theme: {
     extend: {
-      backgroundImage: {
-        'custom-bg': "url('./../assets/img/companie-desktop-01-1.webp')",
-        'custom-bg-form': "url('./../assets/img/img-contact-form-1.webp')",
-        'custom-bg-contact': "url('./../assets/img/hero-contact-desktop-1.webp')",
-        'custom-bg-empresa': "url('./../assets/img/how-it-works-desktop-02.webp')",
-        'custom-bg-como-funciona': "url('./../assets/img/our-food-desktop-01-1.webp')",
-        'custom-bg-price': "url('./../assets/img/how-menus-1.webp')",
-      },
       screens: {
         sm: '480px',
         md: '768px',
@@ -18,14 +10,27 @@ module.exports = {
         xl: '1440px'
       },
       colors: {
+        white: '#FFFFFF',
+        sand: '#F0EAE1',
+        yellow: '#EAC355',
+        madera: '#AF917C',
         negro: '#1B1B1B',
-        yellowsec: '#EAC355',
-        sandclaro: '#F0EAE1',
-        sandoscuro: '#DBD8CE',
+        whiteTextFooter: '#E9E9E9',
+        grayTextFooter: '#909090',
       },
       fontFamily: {
-        sans: ['Roboto', 'sans-serif'],
+        primary: ['Roboto'],
+        secondary: ['Playfair Display'],
+        special: ['Roboto Serif 72pt Condensed'],
+
       },
+      boxShadow: {
+        s100: '0px 4px 4px 0px rgba(167, 176, 192, 0.16)',
+        s200: '0px 8px 12px 0px rgba(167, 176, 192, 0.32)',
+        s300: ' 0px 12px 20px 0px rgba(167, 176, 192, 0.24)',
+        s400: '0px 18px 24px 0px rgba(167, 176, 192, 0.30)',
+        s500: '0px 20px 28px 0px rgba(167, 176, 192, 0.40)',
+      }
     },
   },
   plugins: [],
