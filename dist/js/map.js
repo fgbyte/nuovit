95b5bd39a7b9a6b018ce2e94b6ada9e93ec65bc9
fgbyte <garciabayonf@gmail.com>
2023-08-24 10:58:16 -0400

    working on map styles

--- dist/js/map.js
+++ dist/js/map.js
@@ -11,15 +11,19 @@ function initMap() {
     //map printed on the page
     const map = new google.maps.Map(document.getElementById('map'), {
         zoom: 17,
-        center: position
+        center: position,
     })
+
+    console.log(map)
+
+    const blackMarker = './assets/icons/black-marker.svg'
 
     //map marker
     const marker = new google.maps.Marker({
         position,
+        icon : blackMarker,
         map,
         title: 'Posicion Inicial'
     })
-
 
 }
