e342ccdc09552c84114814a8b048e1c59cf8ba08
fgbyte <garciabayonf@gmail.com>
2023-09-06 16:11:02 -0400

    elipse bak

---
+++ dist/js/thumbs-carousel.js
@@ -0,0 +1,34 @@
+const carousels = document.querySelectorAll(".carousel-container");
+const controlsList = document.querySelectorAll(".guias");
+
+carousels.forEach((carousel, carouselIndex) => {
+  const controls = controlsList[carouselIndex];
+  const controlItems = controls.querySelectorAll("div[data-index]");
+
+  controlItems[0].classList.add("brightness-125");
+
+  function setActiveControl(index) {
+    controlItems.forEach((item, i) => {
+      if (i === index) {
+        item.classList.add("brightness-125");
+      } else {
+        item.classList.remove("brightness-125");
+      }
+    });
+  }
+
+  controlItems.forEach((item, index) => {
+    item.addEventListener("click", () => {
+      const cardWidth = carousel.querySelector(".snap-start").offsetWidth;
+      carousel.scroll({ left: cardWidth * index, behavior: "smooth" });
+      setActiveControl(index);
+    });
+  });
+
+  carousel.addEventListener("scroll", () => {
+    const cardWidth = carousel.querySelector(".snap-start").offsetWidth;
+    const scrollPos = carousel.scrollLeft;
+    const activeControlIndex = Math.floor(scrollPos / cardWidth);
+    setActiveControl(activeControlIndex);
+  });
+});
