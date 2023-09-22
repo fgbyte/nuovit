bbb9fe5e4c8343d86358e83cb56bd4164da70d97
fgbyte <garciabayonf@gmail.com>
2023-09-21 20:33:59 -0400

    fix horizontal scroll

--- dist/js/wizard-form.js
+++ dist/js/wizard-form.js
@@ -1,60 +1,60 @@
 const form = document.getElementById("wizard-form");
 
 form.addEventListener("submit", function (event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    //Objeto editable con los valores del Form
-    const formData = {
-        "edad": {
-            value: document.getElementById("age").value,
-        },
-        "genero": {
-            value: document.getElementById("genre").value,
-        },
-        "postal-code": {
-            value: document.getElementById("postal-code").value,
-        },
-        "hijos": {
-            value: document.getElementById("childrens").value,
-        },
-        "tipo_de_vivienda": {
-            value: document.getElementById("house").value,
-        },
-        "dormitorios": {
-            value: document.getElementById("rooms").value,
-        },
-        "gustaria_tener": {
-            values: {
-                "piscina": document.getElementById("Piscina").checked,
-                "gimnasio": document.getElementById("Gimnasio").checked,
-                "co_Working": document.getElementById("Co-Working").checked,
-                "zonaInfantil": document.getElementById("Zona-Infantil").checked,
-                "garaje": document.getElementById("Plaza-Garaje").checked,
-                "trastero": document.getElementById("Trastero").checked,
-            }
-        },
-        "certificacion_energetica": {
-            value: document.getElementById("energy").value,
-        },
-        "metros_cuadrados": {
-            value: document.getElementById("meters").value,
-        },
-        "presupuesto": {
-            value: document.getElementById("budget").value * 1000,
-        },
-        "uso": {
-            value: document.getElementById("use").value,
-        },
-        "tu_casa_ideal": {
-            value: document.getElementById("message").value,
-        },
-        "email": {
-            value: document.getElementById("email").value,
-        },
-        
-    }
-    //para desarrollo
-    console.log(formData)
+  //Objeto editable con los valores del Form
+  const formData = {
+    "edad": {
+      value: document.getElementById("age").value,
+    },
+    "genero": {
+      value: document.getElementById("genre").value,
+    },
+    "postal-code": {
+      value: document.getElementById("postal-code").value,
+    },
+    "hijos": {
+      value: document.getElementById("childrens").value,
+    },
+    "tipo_de_vivienda": {
+      value: document.getElementById("house").value,
+    },
+    "dormitorios": {
+      value: document.getElementById("rooms").value,
+    },
+    "gustaria_tener": {
+      values: {
+        "piscina": document.getElementById("Piscina").checked,
+        "gimnasio": document.getElementById("Gimnasio").checked,
+        "co_Working": document.getElementById("Co-Working").checked,
+        "zonaInfantil": document.getElementById("Zona-Infantil").checked,
+        "garaje": document.getElementById("Plaza-Garaje").checked,
+        "trastero": document.getElementById("Trastero").checked,
+      }
+    },
+    "certificacion_energetica": {
+      value: document.getElementById("energy").value,
+    },
+    "metros_cuadrados": {
+      value: document.getElementById("meters").value,
+    },
+    "presupuesto": {
+      value: document.getElementById("budget").value * 1000,
+    },
+    "uso": {
+      value: document.getElementById("use").value,
+    },
+    "tu_casa_ideal": {
+      value: document.getElementById("message").value,
+    },
+    "email": {
+      value: document.getElementById("email").value,
+    },
+
+  }
+  //para desarrollo
+  console.log(formData)
 
 });
 
@@ -80,39 +80,45 @@ function scrollBack() {
 }
 
 function updateCurrentPage() {
-    const currentPosition = carrusel.scrollLeft;
-    const cardWidth = carrusel.querySelector("[data-index='0']").clientWidth;
-    const currentPageNumber = Math.floor(currentPosition / cardWidth) + 1;
-    currentPage.innerText = formatearNumero(currentPageNumber);
+  const currentPosition = carrusel.scrollLeft;
+  const cardWidth = carrusel.querySelector("[data-index='0']").clientWidth;
+  const currentPageNumber = Math.floor(currentPosition / cardWidth) + 1;
+  currentPage.innerText = formatearNumero(currentPageNumber);
 
-    if (currentPageNumber === 1) {
-        previousCard.style.opacity = "0";
-        previousCard.style.cursor = 'auto';
-      } else {
-        previousCard.style.opacity = "1";
-        previousCard.style.cursor = 'pointer';
-      }
-    
-      if (currentPageNumber === 14) {
-        nextCard.style.opacity = "0";
-        nextCard.style.cursor = 'auto';
-      } else {
-        nextCard.style.opacity = "1";
-        nextCard.style.cursor = 'pointer';
-    }
-    
+  if (currentPageNumber === 1) {
+    previousCard.style.opacity = "0";
+    previousCard.style.cursor = 'auto';
+  } else {
+    previousCard.style.opacity = "1";
+    previousCard.style.cursor = 'pointer';
   }
+
+  if (currentPageNumber === 14) {
+    nextCard.style.opacity = "0";
+    nextCard.style.cursor = 'auto';
+  } else {
+    nextCard.style.opacity = "1";
+    nextCard.style.cursor = 'pointer';
+  }
+
+}
 
 function formatearNumero(num) {
-    if (num < 10) {
-      return "0" + num.toString();
-    } else {
-      return num.toString();
-    }
+  if (num < 10) {
+    return "0" + num.toString();
+  } else {
+    return num.toString();
   }
+}
 
 nextCard.addEventListener("click", scrollNext);
 previousCard.addEventListener("click", scrollBack);
 carrusel.addEventListener("scroll", updateCurrentPage);
 
 updateCurrentPage();
+
+
+//evitar scroll horizontal
+const slider = document.querySelector(".carousel-container");
+slider.style.overflowX = "hidden"
+slider.style.overflowY = "block"
